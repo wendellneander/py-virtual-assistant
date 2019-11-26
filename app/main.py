cf6f78238ad120d38bcde31702144b3eafacc42b
Wendell Neander <wendell.neander@mirumagency.com>
2019-11-26 11:51:05 -0300

    speak

---
+++ app/main.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+from gtts import gTTS
+import os
+import time
+import playsound
+
+
+def speak(text):
+    tts = gTTS(text=text, lang='en')
+    filename = 'voice.mp3'
+    tts.save(filename)
+    playsound.playsound(filename)
+
+
+speak('Hello world.')
