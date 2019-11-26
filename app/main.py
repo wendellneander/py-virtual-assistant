5f568f04a35e23b22f726e6737fb479fd4453fd3
Wendell Neander <wendell.neander@mirumagency.com>
2019-11-26 17:11:11 -0300

    listen microphone

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
 import speech_recognition as sr
-from gtts import gTTS
-import os
-import time
 import playsound
+from gtts import gTTS
 
 
 def speak(text):
@@ -12,4 +10,25 @@ def speak(text):
     playsound.playsound(filename)
 
 
-speak('Hello world.')
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print('say something, im listening')
+        audio = r.listen(source)
+        said = ''
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print('Exception: ' + str(e))
+
+    return said
+
+
+text = get_audio()
+
+if 'hello' in text:
+    speak('hello, how are you?')
+elif 'what is your name' in text:
+    speak('my name is Jarvis')
